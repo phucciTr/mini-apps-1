75c36edf4a1b88d3e6a884f07abfbcba37b36bc3
phucciTr <miabam26@gmail.com>
2021-02-22 17:04:10 -0800

    Minor edit

--- challenge_3/client/components/Form.jsx
+++ challenge_3/client/components/Form.jsx
@@ -2,6 +2,6 @@ var Form = ({step, next, saveId, userId, addToSummary, summary, getHome}) => {
   if (step === 1) { return <F1 nextForm={next} saveId={saveId} addToSummary={addToSummary} />; }
   if (step === 2) { return <F2 nextForm={next} userId={userId} addToSummary={addToSummary} />; }
   if (step === 3) { return <F3 nextForm={next} userId={userId} addToSummary={addToSummary} />; }
-  if (step === 4) { return <Confirmation nextForm={next} userId={userId} addToSummary={addToSummary} summary={summary} getHome={getHome} />}
+  if (step === 4) { return <Confirmation summary={summary} getHome={getHome} />}
 }
 
